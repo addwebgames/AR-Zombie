901b9733b6e0cd677c1faadaca151aee23edfbaa
Milan <milan.addweb@gmail.com>
2018-05-24 23:44:51 -0700

    AR view & gyro movement

---
+++ Assets/_AR/Makaka Games/AR Camera GYRO/Scripts/GyroCameraControl.cs
@@ -0,0 +1,38 @@
+﻿// =========================
+// MAKAKA GAMES - MAKAKA.ORG
+// =========================
+
+using UnityEngine;
+using UnityEngine.Events;
+
+[HelpURL("https://makaka.org/category/docs")]
+[AddComponentMenu ("AR/GyroCameraControl")]
+public class GyroCameraControl : MonoBehaviour 
+{
+	private Gyroscope gyro;
+	private bool gyroSupported;
+	private Quaternion rotationFix = new Quaternion(0f, 0f, 1f, 0f);
+
+	public Transform gyroCamera;
+
+	void Start () 
+	{
+		gyroSupported = SystemInfo.supportsGyroscope;
+
+		gyroCamera.parent.transform.rotation = Quaternion.Euler(90f, 180f, 0f);
+
+		if (gyroSupported) 
+		{
+			gyro = Input.gyro;
+			gyro.enabled = true;
+		}
+	}
+
+	void Update () 
+	{
+		if (gyroSupported)
+		{
+			gyroCamera.localRotation = gyro.attitude * rotationFix;
+		}
+	}
+}
---
+++ Assets/_Script/Game/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+	public static GameManager Instance;
+
+	#region public-variable
+	public Text txtgyroMessage;
+	#endregion
+
+	#region private-variable
+	#endregion
+
+	#region unity-callback
+	void Awake()
+	{
+		Instance = this;
+
+		if(SystemInfo.supportsGyroscope)
+		{
+			// gyro supported
+			txtgyroMessage.text = "Look around via smartphone";
+		}
+		else
+		{
+			// accelerometer
+			txtgyroMessage.text = "Device doesn't support gyro";
+		}
+	}
+
+	void Start () 
+	{
+		
+	}
+	#endregion
+
+	#region Delagates
+	#endregion
+
+	#region private-function
+	#endregion
+
+	#region public-function
+	#endregion
+
+	#region coroutine
+	#endregion
+}
